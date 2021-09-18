48e05ee506980703c8ef8258cb945a8b5051f8d8
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-09-18 10:35:26 +0900

    Chapter2 データモデルクラスの定義

---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public decimal Revenue { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
