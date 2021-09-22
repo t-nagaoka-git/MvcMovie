a3e74fc12fbd23c9f5766c3c0aacdbc168c8041b
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-09-22 11:02:12 +0900

    Chapter4 Movieモデルに新しいプロパティの追加

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie.Models
@@ -6,11 +7,21 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [DisplayName("タイトル")]
         public string Title { get; set; }
+
+        [DisplayName("ジャンル")]
         public string Genre { get; set; }
+
+        [DisplayName("興行収入（億円）")]
         public decimal Revenue { get; set; }
 
+        [DisplayName("公開日")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [DisplayName("レイティング")]
+        public string Rating { get; set; }
     }
 }
