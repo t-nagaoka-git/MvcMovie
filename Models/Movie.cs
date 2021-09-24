1e843ebbf93e420431ed0c5081b8547b429d6fa0
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-09-24 21:15:35 +0900

    Chapter4 検証属性の追加

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -9,12 +9,18 @@ namespace MvcMovie.Models
         public int Id { get; set; }
 
         [DisplayName("タイトル")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "3文字以上、60文字以内で入力してください。")]
+        [Required(ErrorMessage = "入力必須です。")]
         public string Title { get; set; }
 
         [DisplayName("ジャンル")]
+        [StringLength(30)]
+        [Required(ErrorMessage = "入力必須です。")]
         public string Genre { get; set; }
 
         [DisplayName("興行収入（億円）")]
+        [DataType(DataType.Currency)]
+        [Range(1, 1000)]
         public decimal Revenue { get; set; }
 
         [DisplayName("公開日")]
@@ -22,6 +28,9 @@ namespace MvcMovie.Models
         public DateTime ReleaseDate { get; set; }
 
         [DisplayName("レイティング")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required(ErrorMessage = "入力必須です。")]
         public string Rating { get; set; }
     }
 }
